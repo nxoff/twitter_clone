895ebf9cdc0ea7666be400ab720515ce65ac131b
Narcikss <mdgamedeveloper@gmail.com>
2024-04-25 11:09:01 +0200

    rel

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,10 +1,27 @@
 import Image from 'next/image';
 
+import '../../styles/sign-pages.css';
+
 const page = () => {
 	return (
-		<div>
-			<h1>Sign in</h1>
-		</div>
+		<main className='main-sign-in'>
+			<div className='left-bar'>
+				<Image src='/x-main-logo.png' alt='' width={300} height={280} />
+			</div>
+			<div className='right-bar'>
+				<header>
+					<h1>Najświeższe wieści ze świata</h1>
+					<span>Dołącz już dziś.</span>
+				</header>
+				<form className='sign-in-form'>
+					<span>Nazwa użytkownika</span>
+					<input type='text' placeholder='username' />
+					<span>Hasło</span>
+					<input type='text' placeholder='password' />
+					<input type='submit' value='Zaloguj się' />
+				</form>
+			</div>
+		</main>
 	);
 };
 
